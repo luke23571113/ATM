441f2befb807e090608d5f2ce76ec4fdc304075f
Luke Collins <luke23571113@gmail.com>
2023-08-31 14:03:39 -0700

    added function skeleton

---
+++ ATM.java
@@ -0,0 +1,47 @@
+import java.util.*;
+import java.io.*;
+
+public class ATM
+{
+    private HashMap <String, Double> accounts;
+
+    public ATM ()
+    {
+        accounts = new HashMap<String, Double> ();
+    }
+
+    public void openAccount (String userId, double amount)
+    {
+
+    }
+
+    public void closeAccount (String userId)
+    {
+
+    }
+
+    public double checkBalance (String userId)
+    {
+        return accounts.get (userId);
+    }
+
+    public double depositMoney (String userId, double amount)
+    {
+        return -1;
+    } 
+
+    public double withdrawMoney (String userId, double amount)
+    {
+        return -1;
+    }
+
+    public boolean transferMoney (String fromAccount, String toAccount, double amount)
+    {
+        return false; 
+    }
+
+    public void audit ()
+    {
+        
+    }   
+}
